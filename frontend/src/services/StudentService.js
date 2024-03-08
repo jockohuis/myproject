a675a5d80f2423dd486215828a77f8f74796c501
jockohuis <jockohuis@gmail.com>
2024-03-08 20:11:31 +0100

    V.1.1.0

---
+++ frontend/src/services/StudentService.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+export function getStudents() {
+  return axios.get('http://192.168.49.2:30984/students/')
+    .then(response => response.data)
+}
+
+export function deleteStudent(studentId) {
+  return axios.delete('http://192.168.49.2:30984/students/' + studentId + '/', {
+   method: 'DELETE',
+   headers: {
+     'Accept':'application/json',
+     'Content-Type':'application/json'
+   }
+  })
+  .then(response => response.data)
+}
+
+export function addStudent(student){
+  return axios.post('http://192.168.49.2:30984/students/', {
+    studentId:null,
+    FirstName:student.FirstName.value,
+    LastName:student.LastName.value,
+    RegistrationNo:student.RegistrationNo.value,
+    Email:student.Email.value,
+    Course:student.Course.value
+  })
+    .then(response=>response.data)
+}
+
+export function updateStudent(stuid, student) {
+  return axios.put('http://192.168.49.2:30984/students/' + stuid + '/', {
+    FirstName:student.FirstName.value,
+    LastName:student.LastName.value,
+    RegistrationNo:student.RegistrationNo.value,
+    Email:student.Email.value,
+    Course:student.Course.value
+  })
+   .then(response => response.data)
+}
+
